c343925c731cf408c410e755f0fce3b8f559547b
Akarshit Mahendru <akarshit.mahendru@jaarvis.com>
2021-08-04 15:29:08 +0530

    url change

--- listings/urls.py
+++ listings/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import apis
 
 urlpatterns = [
-    url(r'^available-rooms/$', apis.AvailableRoomsAPI.as_view(), name='rooms'),
+    url(r'^units/$', apis.AvailableRoomsAPI.as_view(), name='rooms'),
 
 ]
